21ad7e6988c3a393e87968abb00ea1d030d1aa25
Mykola Mazurenko <61743399+ZametkiPustoty@users.noreply.github.com>
2022-12-26 19:15:28 +0200

    added v2, my scripts ButtonAll and animator Button_AC ( Flush and Fill)

---
+++ Assets/Resources/UI/Scripts/ButtonAll.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonAll : MonoBehaviour
+{
+    public GameObject Box;
+
+    private void ExecuterTrigger(string trigger)
+    {
+        if (Box != null)
+        {
+            var animator = Box.GetComponent<Animator>();
+            if (animator != null)
+            {
+                animator.SetTrigger(trigger);
+            }
+
+        }
+        
+    }
+    public void OnOpenButtonClick()
+    {
+        ExecuterTrigger("TrOpen");
+    }
+    public void OnCloseButtonClick()
+    {
+        ExecuterTrigger("TrClose");
+    }
+}
